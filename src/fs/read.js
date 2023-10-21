5eea24babd271ccd6244d381fb473a2c8262b001
KohnoA <san.kohno@yandex.ru>
2023-10-21 22:23:36 +0300

    feat: completed fs:read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    fs.readFile(pathToFile, 'utf-8', (err, data) => {
+        if (err) throw new Error('FS operation failed');
+
+        console.log(data);
+    });
 };
 
 await read();
