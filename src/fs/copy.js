402aefa5c7131f060118d62a8470a012d392d3bd
KohnoA <san.kohno@yandex.ru>
2023-10-21 19:41:16 +0300

    feat: completed copy and create tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const inputFolderPath = path.join(__dirname, "files");
+  const outputFolderPath = path.join(__dirname, "files_copy");
+
+  fs.readdir(inputFolderPath, "utf-8", (err, files) => {
+    if (err?.code === 'ENOENT') throw new Error('FS operation failed');
+
+    fs.mkdir(outputFolderPath, (err) => {
+      if (err?.code === 'EEXIST') throw new Error('FS operation failed');;
+    });
+
+    files.forEach((file) => {
+      fs.copyFile(
+        path.join(inputFolderPath, file),
+        path.join(outputFolderPath, file),
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    });
+  });
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,38 @@
+import fs, { constants } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filename = "fresh.txt";
+  const filepath = path.join(__dirname, "files", filename);
+
+  fs.writeFile(filepath, "I am fresh and young", { flag: 'wx' }, (err) => {
+    if (err?.code === 'EEXIST') throw new Error('FS operation failed');
+  });
+
+  // fs.access(filepath, constants.F_OK, (err) => {
+  //     if (err) {
+  //         fs.writeFile(filepath, 'I am fresh and young', (err) => {
+  //             if (err) throw err;
+  //         });
+  //     } else {
+  //         throw new Error('FS operation failed');
+  //     }
+  // });
 };
 
 await create();
+
+/* Comments */
+/*
+    fileURLToPath - нормализация пути для текущего ОС
+    import.meta - объект с метаданными модуля
+    path.dirname - вернет путь к директории файла по указанному пути
+    access - проверить файл
+    constant.F_OK - мод проверки (существует или нет, есть и други моды)
+    writeFile - создать если нет и записать в файл или заменить то что в файле
+    wx - если путь уже существует, то выбрасывает ошибку
+*/
