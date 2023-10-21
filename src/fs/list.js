ff751da6a5b887e93e998b41065da39a794d703b
KohnoA <san.kohno@yandex.ru>
2023-10-21 22:16:54 +0300

    feat: completed list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const pathToDir = path.join(__dirname, 'files');
+
+  fs.readdir(pathToDir, (err, files) => {
+    if (err) throw new Error('FS operation failed');
+
+    console.log(files);
+  });
 };
 
 await list();
