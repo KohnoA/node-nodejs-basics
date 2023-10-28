a9018afb26a11e4764edaa2e582f38bb0327e4fd
KohnoA <san.kohno@yandex.ru>
2023-10-28 15:16:54 +0300

    feat: complete cli:args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const VAR_DESIGNATION = '--';
+  const passedArgs = argv.slice(2, argv.length);
+
+  const result = passedArgs.reduce((acc, next, index, thisArr) => {
+    if (next.includes(VAR_DESIGNATION)) {
+      const key = next.replace(VAR_DESIGNATION, '');
+      const value = thisArr[index + 1];
+
+      acc.push(`${key} is ${value}`);
+    }
+
+    return acc;
+  }, []).join(', ');
+
+  console.log(result);
 };
 
 parseArgs();
