82712cbaf1855843868927ea978bb532fa2a0d5a
KohnoA <san.kohno@yandex.ru>
2023-11-12 15:49:20 +0300

    completed stream:read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { stdout } from 'node:process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const pathToFile = join(__dirname, 'files', 'fileToRead.txt');
+  const readStream = createReadStream(pathToFile, { encoding: 'utf-8' });
+
+  readStream.on('data', (chunk) => stdout.write(chunk));
+
+  readStream.on('error', (err) => {
+    throw new Error('Streams operation failed', err);
+  });
 };
 
 await read();
