c7f0bb8fff37de24aeb68216208bcdc10d15444a
KohnoA <san.kohno@yandex.ru>
2023-11-12 16:25:51 +0300

    feat: completed streams:write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import { stdin } from 'node:process';
+import { createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToWrite.txt');
+    const writeStream = createWriteStream(pathToFile, { encoding: 'utf-8' });
+
+    // stdin.on('data', (chunk) => writeStream.write(chunk));
+    stdin.pipe(writeStream); // Синхронизация между потом чтения и записи
+    stdin.on('error', (err) => {
+        throw new Error('Streams operation failed', err);
+    });
 };
 
 await write();
