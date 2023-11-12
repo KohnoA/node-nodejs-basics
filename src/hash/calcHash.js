24148f0ba782aa7bf1bf3befdfdb2c9f36e45df8
KohnoA <san.kohno@yandex.ru>
2023-11-12 15:22:11 +0300

    feat: completed hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import crypto from 'node:crypto';
+import fsPromises from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = crypto.createHash('sha256');
+
+  try {
+    const data = await fsPromises.readFile(filePath, { encoding: 'utf-8' });
+  
+    hash.update(data);
+  
+    console.log(hash.digest('hex'));
+  } catch (err) {
+    throw new Error('Hash operation failed');
+  }
+
 };
 
 await calculateHash();
