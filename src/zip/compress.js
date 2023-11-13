7adf3fe706c623b426da743c2550596bd52dffc1
KohnoA <san.kohno@yandex.ru>
2023-11-13 19:04:00 +0300

    feat: completed zip:compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import zlib from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const inputPath = join(__dirname, 'files', 'fileToCompress.txt');
+  const outputPath = join(__dirname, 'files', 'archive.gz');
+
+  const gzip = zlib.createGzip();
+  const readStream = createReadStream(inputPath);
+  const writeStream = createWriteStream(outputPath);
+
+  pipeline(readStream, gzip, writeStream, (err) => {
+    if (err) throw new Error(`Zip operation failer ${err}`);
+  });
 };
 
 await compress();
