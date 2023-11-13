a1ba0adc8f5b130ecb62fa99cc1ea384e2c07bd9
KohnoA <san.kohno@yandex.ru>
2023-11-13 23:45:29 +0300

    feat: completed zip:decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import zlib from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { dirname, join, basename } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const inputPath = join(__dirname, 'files', 'archive.gz');
+  const outputPath = join(__dirname, 'files', 'fileToDecompress.txt');
+
+  const gunzip = zlib.createGunzip();
+  const readStream = createReadStream(inputPath);
+  const writeStream = createWriteStream(outputPath);
+
+  pipeline(readStream, gunzip, writeStream, (err) => {
+    if (err) throw new Error(`Zip operation failed ${err}`);
+  });
 };
 
 await decompress();
