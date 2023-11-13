d65ffde1ecd002e0e4489e23aed7b3b124e92bb3
KohnoA <san.kohno@yandex.ru>
2023-11-13 18:41:40 +0300

    feat: completed streams:transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import { Transform, pipeline } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const chunkStringified = chunk.toString().trim();
+      const reversedData = chunkStringified.split('').reverse().join('');
+
+      this.push(reversedData + '\n');
+
+      // callback(null, reversedData + '\n');
+      callback();
+    }
+  });
+
+  pipeline(
+    stdin,
+    transformStream,
+    stdout,
+    (err) => {
+      if (err) throw new Error(`Streams operation failed: ${err}`);
+    }
+  )
 };
+
 
 await transform();
