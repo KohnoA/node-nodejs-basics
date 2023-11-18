a4a1ac9bf9122b5925024fc309e4d6f099118c70
KohnoA <san.kohno@yandex.ru>
2023-11-19 01:02:42 +0300

    feat: completed task cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fork } from 'node:child_process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathToFile = join(__dirname, 'files', 'script.js');
+
+  fork(pathToFile, args);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['one', 'two', 'three']);
